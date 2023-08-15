cda1177a0d68869b44e91ecc38bd4e2ae3cbea91
Juniper Parsons <juniperparsnips@gmail.com>
2023-08-14 22:30:28 -0400

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 //! A state machine thing for animating sprites with bevy.
 // #![deny(warnings)]
 #![warn(missing_docs)]
+/// The base logic for switching between animation states
 pub mod state_machine;
+/// The types of states that can be switched between
 pub mod states;
 
 #[cfg(feature = "bevy")]
+/// Plugin support for the Bevy engine
 pub mod bevy;
